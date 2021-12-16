d394843477dbe7c162504196280858ae43946713
Kazumichi Yamamoto <yamamoto.febc@gmail.com>
2021-12-16 21:10:49 +0900

    Initial commit

---
+++ Makefile
@@ -0,0 +1,45 @@
+default: run
+
+DOCKER_PLATFORM ?= linux/amd64
+RUST_VERSION    ?= 1.57
+RUST_ARCH       ?= x86_64-unknown-linux-gnu
+DEPS            ?= bootstrap/src/*.rs bootstrap/Cargo.toml Cargo.toml Cargo.lock
+
+RELEASE_DIR     := ${PWD}/target/${RUST_ARCH}/release
+TARGET_BIN      := ${RELEASE_DIR}/bootstrap
+TARGET_ZIP      := lambda.zip
+
+build:
+	cargo build --release --target ${RUST_ARCH}
+
+.PHONY: buildx
+buildx: $(TARGET_BIN)
+
+$(TARGET_BIN): $(DEPS)
+	docker run -it --rm --platform ${DOCKER_PLATFORM} \
+	  -v "$${PWD}":/usr/src/myapp -w /usr/src/myapp rust:${RUST_VERSION} \
+	  make build
+
+.PHONY: run
+run: $(TARGET_BIN)
+	cat event.json | docker run -i --rm \
+	  --user "$(id -u)":"$(id -g)" \
+	  -e DOCKER_LAMBDA_USE_STDIN=1 \
+	  -e RUST_BACKTRACE=1 \
+	  -e SLACK_POST_URL \
+	  -e SLACK_CHANNEL \
+	  -v "${RELEASE_DIR}":/var/task lambci/lambda:provided.al2
+
+.PHONY: zip
+zip:  $(TARGET_ZIP)
+$(TARGET_ZIP): $(TARGET_BIN)
+	zip -j "$(TARGET_ZIP)" "$(TARGET_BIN)"
+
+.PHONY: clean
+clean:
+	rm -f "$(TARGET_ZIP)"; \
+	cargo clean
+
+.PHONY: fmt
+fmt:
+	cargo fmt
--- bootstrap/src/main.rs
+++ bootstrap/src/main.rs
@@ -1,15 +1,100 @@
+use chrono::{DateTime, Duration, Utc};
 use lambda_runtime::{handler_fn, Context, Error};
+use rusoto_cloudwatch::{
+    CloudWatch, CloudWatchClient, Dimension, GetMetricStatisticsError, GetMetricStatisticsInput,
+    GetMetricStatisticsOutput,
+};
+use rusoto_core::{Region, RusotoError};
 use serde_json::{json, Value};
+use simplelog::{Config, LevelFilter};
+use std::env;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
+    simplelog::SimpleLogger::init(LevelFilter::Info, Config::default()).unwrap();
+
     let func = handler_fn(func);
     lambda_runtime::run(func).await?;
     Ok(())
 }
 
-async fn func(event: Value, _: Context) -> Result<Value, Error> {
-    let first_name = event["firstName"].as_str().unwrap_or("world");
+async fn func(_: Value, _: Context) -> Result<Value, Error> {
+    let statistics = get_estimated_charges_from_cloudwatch().await?;
+    let attachment = build_message(statistics)?;
+    post_message_to_slack(attachment).await
+}
 
-    Ok(json!({ "message": format!("Hello, {}!", first_name) }))
+async fn post_message_to_slack(attachment: Value) -> Result<Value, Error> {
+    let slack_post_url = env::var("SLACK_POST_URL").unwrap();
+    let slack_channel = env::var("SLACK_CHANNEL").unwrap();
+    log::info!(
+        "start: slackPostURL:{}, slackChannel:{}",
+        slack_post_url,
+        slack_channel
+    );
+
+    let body = json!({
+        "channel": slack_channel.to_string(),
+        "attachments": vec![attachment],
+    });
+
+    let client = reqwest::Client::new();
+    client
+        .post(slack_post_url.to_string())
+        .body(body.to_string())
+        .send()
+        .await?;
+
+    Ok(json!({
+        "message": format!("Send message to {}{}", slack_post_url, slack_channel)
+    }))
+}
+
+fn build_message(statistics: GetMetricStatisticsOutput) -> Result<Value, Error> {
+    let points = statistics.datapoints.unwrap();
+    let dp = points.get(0).unwrap();
+
+    let date = DateTime::parse_from_rfc3339(dp.timestamp.as_ref().unwrap().as_str())
+        .unwrap()
+        .format("%Y年%m月%d日")
+        .to_string();
+    let cost = dp.maximum.unwrap_or(0.0);
+
+    let color = match cost {
+        n if (n < 0.5) => "good",               // green
+        n if (0.5 < n && n < 1.0) => "warning", // yellow
+        _ => "#ff0000",                         // red
+    };
+
+    let text = format!("{}までのAWSの料金は、${}です。", date, cost);
+    return Ok(json!({
+        "text": text,
+        "color": color,
+    }));
+}
+
+async fn get_estimated_charges_from_cloudwatch(
+) -> Result<GetMetricStatisticsOutput, RusotoError<GetMetricStatisticsError>> {
+    let client = CloudWatchClient::new(Region::UsEast1);
+
+    client
+        .get_metric_statistics(GetMetricStatisticsInput {
+            namespace: String::from("AWS/Billing"),
+            metric_name: String::from("EstimatedCharges"),
+
+            dimensions: Some(vec![Dimension {
+                name: String::from("Currency"),
+                value: String::from("USD"),
+            }]),
+
+            start_time: (Utc::today().and_hms(0, 0, 0) - Duration::days(1))
+                .format("%+")
+                .to_string(),
+            end_time: Utc::today().and_hms(0, 0, 0).format("%+").to_string(),
+
+            period: 86400,
+            statistics: Some(vec![String::from("Maximum")]),
+            ..GetMetricStatisticsInput::default()
+        })
+        .await
 }
